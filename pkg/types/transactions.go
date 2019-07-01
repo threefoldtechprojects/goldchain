ce8f9582a38b9bc87f1c3add7d5d5306f5948e5d
Glen De Cauwsemaecker <contact@glendc.com>
2019-07-01 20:18:20 +0200

    type tx versions and update rivine ver

--- pkg/types/transactions.go
+++ pkg/types/transactions.go
@@ -1,14 +1,16 @@
 package types
 
+import "github.com/threefoldtech/rivine/types"
+
 const (
 	//MinterDefinitionTxVersion is the transaction version for the   minterdefinition transaction
-	MinterDefinitionTxVersion = iota + 128
+	MinterDefinitionTxVersion types.TransactionVersion = iota + 128
 	//CoinCreationTxVersion is the transaction version for the coin creation transaction
 	CoinCreationTxVersion
 )
 
 // Auth Coin Tx Extension Transaction Versions
 const (
-	TransactionVersionAuthAddressUpdateTx = iota + 176
+	TransactionVersionAuthAddressUpdateTx types.TransactionVersion = iota + 176
 	TransactionVersionAuthConditionUpdateTx
 )

threefoldtechprojects/goldchain

commit bfd810cba30c57fb7e93ce047d867da62099d909

Close plugins if there is an error during registration

cmd/goldchaind/daemon.go
@@ -139,6 +139,10 @@ func runDaemon(cfg ExtendedDaemonConfig, moduleIdentifiers daemon.ModuleIdentifi
 			err = cs.RegisterPlugin(ctx, "authcointx", authCoinTxPlugin)
 			if err != nil {
 				servErrs <- fmt.Errorf("failed to register the auth coin tx extension: %v", err)
+				err = authCoinTxPlugin.Close() //make sure any resources are released
+				if err != nil {
+					fmt.Println("Error during closing of the authCoinTxPlugin :", err)
+				}
 				cancel()
 				return
 			}
@@ -154,6 +158,10 @@ func runDaemon(cfg ExtendedDaemonConfig, moduleIdentifiers daemon.ModuleIdentifi
 			err = cs.RegisterPlugin(ctx, "minting", mintingPlugin)
 			if err != nil {
 				servErrs <- fmt.Errorf("failed to register the minting extension: %v", err)
+				err = mintingPlugin.Close() //make sure any resources are released
+				if err != nil {
+					fmt.Println("Error during closing of the mintingPlugin :", err)
+				}
 				cancel()
 				return
 			}
@@ -302,8 +310,7 @@ func runDaemon(cfg ExtendedDaemonConfig, moduleIdentifiers daemon.ModuleIdentifi
 		fmt.Println("\rCaught stop signal, quitting...")
 		srv.Close()
 	case <-ctx.Done():
-		fmt.Println("\rContext is done, quitting...")
-		fmt.Println("context is done, quitting...")
+		fmt.Println("\rContext is done, quitting..."
 	}
 
 	cancel()

.gitignore
@@ -15,3 +15,4 @@ cmd/goldchainc/goldchainc
 
 # release dir
 release/
+cmd/goldchaind/testnet

commit ebba290a2d08976ab5f33bd6d9369576c79ab2b3

Fix syntax error
Introduced in commit bfd810cba30c57fb7e93ce047d867da62099d909

cmd/goldchaind/daemon.go
@@ -310,7 +310,7 @@ func runDaemon(cfg ExtendedDaemonConfig, moduleIdentifiers daemon.ModuleIdentifi
 		fmt.Println("\rCaught stop signal, quitting...")
 		srv.Close()
 	case <-ctx.Done():
-		fmt.Println("\rContext is done, quitting..."
+		fmt.Println("\rContext is done, quitting...")
 	}
 
 	cancel()